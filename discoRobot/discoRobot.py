9498103b7a0f2270c40188c3f88c7a82e058074c
Piotr Zdzieblowski <piotr@zdzieblowski.com>
2021-10-24 06:12:25 +0200

    cleaning up the code

--- discoRobot/discoRobot.py
+++ discoRobot/discoRobot.py
@@ -10,19 +10,19 @@ import pyimgur
 
 ##
 
-with open('config.json') as config_file:
+with open("config.json") as config_file:
         config_data = json.load(config_file)
 
-version = '0.0.3'
+version = "0.0.3"
 logging = True
 
 ##
 
 client = discord.Client()
 
-g = TenGiphPy.Giphy(token=config_data["giphy_key"])
+g = TenGiphPy.Giphy(token = config_data["giphy_key"])
 i = pyimgur.Imgur(config_data["imgur_id"])
-t = TenGiphPy.Tenor(token=config_data["tenor_key"])
+t = TenGiphPy.Tenor(token = config_data["tenor_key"])
 
 ##
 
@@ -32,84 +32,85 @@ is_ready = False
 
 def get_guild_names():
         guilds = client.guilds
-        list = ''
+        list = ""
         for g in range(0, len(guilds)):
-                list += '{0}'.format(guilds[g])
+                list += "{0}".format(guilds[g])
                 if g != len(guilds)-1:
-                        list += ' | '
-        return '[ {0} ]'.format(list)
+                        list += " | "
+        return "[ {0} ]".format(list)
 
 def imgur_search(query):
+        igur_image = ""
+
         igur_search = i.search_gallery(query)
 
-        result = ""
-
         igur_item = igur_search[random.randrange(len(igur_search))]
-        if type(igur_item) is pyimgur.Gallery_image:
-                result = igur_item.link
-        else:
-                result = igur_item.images[random.randrange(len(igur_item.images))].link
 
-        return result
+        if type(igur_item) is pyimgur.Gallery_image:
+                igur_image = igur_item.link
+        else:
+                igur_image = igur_item.images[random.randrange(len(igur_item.images))].link
+
+        return igur_image
 
 ##
 
 @client.event
 async def on_ready():
         global is_ready
-        print('Logged in as {0.user.name} on {1}\nWebSocket latency: {0.latency}s\n'.format(client, get_guild_names()))
+        print("Logged in as {0.user.name} on {1}\nWebSocket latency: {0.latency}s\n".format(client, get_guild_names()))
         is_ready = True
 
 @client.event
 async def on_message(message):
         if is_ready:
                 message_channel = client.get_channel(message.channel.id)
-                type = ''
-                content = ''
+                type = ""
+                content = ""
                 if isinstance(message_channel, DMChannel):
-                        details = '[ {0.channel} / {0.author} ]'.format(message)
+                        details = "[ {0.channel} / {0.author} ]".format(message)
                 else:
-                        details = '[ {0.guild} / {0.channel.name} / {0.author} / {0.author.nick} ]'.format(message)
+                        details = "[ {0.guild} / {0.channel.name} / {0.author} / {0.author.nick} ]".format(message)
                 if message.author != client.user:
                         message_content = message.content
 
-                        if message_content.startswith('$'):
-                                type = '<CmdSuccess>'
+                        if message_content.startswith("$"):
+                                type = "<CmdSuccess>"
 
-                                command_split = message_content.split(' ');
+                                command_split = message_content.split(" ");
                                 command = command_split[0]
                                 command_attr = command_split[1:len(command_split)];
 
-                                aliases = {'tenor': ['$tenor', '$t'], 'giphy': ['$giphy', '$g'], 'imgur': ['$imgur','$i']}
+                                aliases = {"tenor": ["$tenor", "$t"], "giphy": ["$giphy", "$g"], "imgur": ["$imgur","$i"], "echo": ["$echo"]}
 
-                                return_message = ''
+                                return_message = ""
 
-                                if command == '$help':
+                                if command == "$help":
                                         return_message = "> **discoRobot**\n\n*version {0}*\n\n```asciidoc\n{1} [attr] :: shows random result for `[attr]' search on Giphy\n{2} [attr] :: shows random result for `[attr]' search on Imgur\n{3} [attr] :: shows random result for `[attr]' search on Tenor```".format(version, aliases['giphy'], aliases['imgur'], aliases['tenor'])
-                                elif command == '$echo':
-                                        return_message = '``ECHO: {0}``'.format(command_attr)
-                                elif command in aliases['giphy']:
-                                        ggif = g.random(tag = ' '.join(command_attr))['data']['images']['downsized_large']['url']
+                                elif command in aliases["echo"]:
+                                        return_message = "``ECHO: {0}``".format(command_attr)
+                                elif command in aliases["giphy"]:
+                                        ggif = g.random(tag = " ".join(command_attr))["data"]["images"]["downsized_large"]["url"]
                                         return_message = '{0}'.format(ggif)
-                                elif command in aliases['imgur']:
-                                        igur = imgur_search(' '.join(command_attr))
-                                        return_message = '{0}'.format(igur)
-                                elif command in aliases['tenor']:
-                                        tgif = t.random(' '.join(command_attr))
-                                        return_message = '{0}'.format(tgif)
+                                elif command in aliases["imgur"]:
+                                        igur = imgur_search(" ".join(command_attr))
+                                        return_message = "{0}".format(igur)
+                                elif command in aliases["tenor"]:
+                                        tgif = t.random(" ".join(command_attr))
+                                        return_message = "{0}".format(tgif)
                                 else:
-                                        type = '<CmdFailure>'
-                                        return_message = '``UNKNOWN COMMAND "{0}"``'.format(command)
+                                        type = "<CmdFailure>"
+                                        return_message = "``UNKNOWN COMMAND '{0}'``".format(command)
 
                                 await message_channel.send(return_message)
 
                         else:
-                                type = '<Message>'
+                                type = "<Message>"
                         if logging:
-                                content = ': {0}'.format(message.content)
+                                content = ": {0}".format(message.content)
                         print('{0} {1} {2}'.format(type,details,content))
 
 ##
 
-print('// discoRobot v{0} [API v{1.major}.{1.minor}.{1.micro}:{1.serial} {1.releaselevel}]\n'.format(version,discord.version_info))
+print("// discoRobot v{0} [API v{1.major}.{1.minor}.{1.micro}:{1.serial} {1.releaselevel}]\n".format(version,discord.version_info))
 client.run(config_data["discord_key"])
